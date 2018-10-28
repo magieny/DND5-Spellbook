23d2a4e886725583e89952e2836c2280523a724e
Shazovi <tgolinski55@gmail.com>
2018-10-28 18:23:14 +0100

    Add new profile list with resources

--- app/src/main/java/com/example/pchmiel/dnd5_spellbook/ProfileListView.java
+++ app/src/main/java/com/example/pchmiel/dnd5_spellbook/ProfileListView.java
@@ -1,15 +1,19 @@
 package com.example.pchmiel.dnd5_spellbook;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
 
 public class ProfileListView extends AppCompatActivity {
 
@@ -18,13 +22,19 @@ public class ProfileListView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTitle(R.string.header_profiles);
+
         setContentView(R.layout.activity_profile_list_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        ListView listView = (ListView)findViewById(R.id.profiles_list);
 
 
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -34,7 +44,6 @@ public class ProfileListView extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         menuItem.setChecked(true);
                         mDrawerLayout.closeDrawers();
-
                         //TODO: PLACE WHERE SCREENS WILL BE CHANGED AFTER TAPPING ITEM ON MENU
 
                         return true;
@@ -47,12 +56,27 @@ public class ProfileListView extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
         switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
+
+        if(id == R.id.action_addNewProfile){
+            Toast.makeText(ProfileListView.this,"Added new profile", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 }
